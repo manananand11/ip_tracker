beab2061946b79a58dde6b3bf9a4a367edccdd58
Manan Anand <manananand11@gmail.com>
2021-11-13 01:31:25 +0530

    dashboard heading

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import "./dashboard.css"
+import Input from '../../components/input/Input'
 
 export default function Dashboard() {
     return (
         <div className="dashboardContainer">
-            
+            <h1 className="dashboardHeading">IP Address Tracker</h1>
+            <Input />
         </div>
     )
 }
