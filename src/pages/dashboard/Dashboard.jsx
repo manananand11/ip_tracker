1fd26ab5cd9309269e5b4b03a373f14b2420b856
Manan Anand <manananand11@gmail.com>
2021-11-13 02:39:28 +0530

    location component added

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,21 @@
 import "./dashboard.css"
 import Input from '../../components/input/Input'
+import Location from "../../components/location/Location"
 
 export default function Dashboard() {
     return (
         <div className="dashboardContainer">
+            <div className="dashboardTop">
             <h1 className="dashboardHeading">IP Address Tracker</h1>
             <Input />
+            </div>
+            <div className="dashboardMiddle">
+
+            </div>
+            <div className="dashboardBottom">
+            <Location />
+            </div>
+            
         </div>
     )
 }
