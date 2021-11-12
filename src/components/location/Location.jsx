f45bb53cf6b94d5fef65751c6244719d4de2629d
Manan Anand <manananand11@gmail.com>
2021-11-13 03:16:09 +0530

    sawo login added

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -4,12 +4,12 @@ import './location.css'
 export default function Location() {
     return (
         <div className="locationContainer">
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[28.5834, 77.1681]} zoom={13} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
+            <Marker position={[28.5834, 77.1681]}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Sawo from 'sawo'
 
-export default function Login() {
+const Login = () => {
+    useEffect(() => {
+        var config = {
+            // should be same as the id of the container created on 3rd step
+            containerID: 'sawo-container',
+            // can be one of 'email' or 'phone_number_sms'
+            identifierType: 'email',
+            // Add the API key copied from 5th step
+            apiKey: 'c7a39dd4-cf2d-45b7-ab17-4f47dec131fa',
+            // Add a callback here to handle the payload sent by sdk
+            onSuccess: payload => {
+                // you can use this payload for your purpose
+            },
+        }
+        let sawo = new Sawo(config)
+        sawo.showForm()
+    }, [])
+
     return (
         <div>
-            This is login
+            <div id="sawo-container" style={{height:"300px", width:"400px"}}></div>
         </div>
     )
 }
+
+export default Login
